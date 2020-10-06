cc67de5fd74d9a9fbce0e1deb8c79a039edcd98d
윤지혜 <yoonjihye@yunjihyeui-MacBook-Pro.local>
2020-10-06 16:48:40 +0900

    calendar card 리스트: 회사이름 comment out - 자리가 없음

--- src/components/commons/Section/Calendar_card.js
+++ src/components/commons/Section/Calendar_card.js
@@ -2,9 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 function CalendarCard(props) {
+
+    const symbol = props.symbol;
+    const data = props.data[symbol];
+
     return (
         <div className={props.className}>
-            {props.symbol}
+            {/* <span className="co_name">{data.name}-</span> */}
+            <span className="co_symbol">{symbol}</span>
         </div>
     )
 }
@@ -12,4 +17,19 @@ function CalendarCard(props) {
 export default styled(CalendarCard)`
     border: 1px solid blue;
     /* margin: 5px 0; */
+    display: flex;
+    align-items: center;
+
+    & {
+        /* .co_name {
+            border: 1px solid red;
+            white-space: nowrap;
+            overflow: hidden;
+            max-width: 80%;
+            text-overflow: ellipsis;
+        } */
+        .co_symbol {
+            /* border: 1px solid green; */
+        }
+    }
 `;
