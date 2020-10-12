26a5780a83244e130763df032ba3993eaf625a6a
윤지혜 <yoonjihye@yunjihyeui-MacBook-Pro.local>
2020-10-12 17:34:35 +0900

    Updates

---
+++ precache-manifest.662b4f22096bd7a8d58835ff9691654a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c381728b7162b644556decb39bd50553",
+    "url": "/Jurini-Web/index.html"
+  },
+  {
+    "revision": "f25877463c942ddce3d2",
+    "url": "/Jurini-Web/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5c6bf0837fcc2f178571",
+    "url": "/Jurini-Web/static/js/2.622ca867.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Jurini-Web/static/js/2.622ca867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f25877463c942ddce3d2",
+    "url": "/Jurini-Web/static/js/main.9154e17f.chunk.js"
+  },
+  {
+    "revision": "7c4b93f403a10aa34ac6",
+    "url": "/Jurini-Web/static/js/runtime-main.4220d0de.js"
+  },
+  {
+    "revision": "91fc682d2268c4141d42b22b1e343008",
+    "url": "/Jurini-Web/static/media/no-data.91fc682d.png"
+  }
+]);
