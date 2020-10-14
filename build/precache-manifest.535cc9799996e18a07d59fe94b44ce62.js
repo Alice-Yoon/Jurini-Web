1da122015f2cb5ea573497b2e288e8185e6cbe6e
윤지혜 <yoonjihye@yunjihyeui-MacBook-Pro.local>
2020-10-15 00:33:34 +0900

    build

---
+++ build/precache-manifest.535cc9799996e18a07d59fe94b44ce62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777d80187bde4322ae5d4bc4959a1de3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55e90a255e19377fee5f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dfa5db3878cdc805d0bb",
+    "url": "/static/js/2.f5499382.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f5499382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55e90a255e19377fee5f",
+    "url": "/static/js/main.688da22a.chunk.js"
+  },
+  {
+    "revision": "75719d11218c5acabac3",
+    "url": "/static/js/runtime-main.8e3d5b26.js"
+  },
+  {
+    "revision": "91fc682d2268c4141d42b22b1e343008",
+    "url": "/static/media/no-data.91fc682d.png"
+  }
+]);
