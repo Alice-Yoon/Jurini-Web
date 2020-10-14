49299c82578f62284206a93ff59e25c7f5decea1
윤지혜 <yoonjihye@yunjihyeui-MacBook-Pro.local>
2020-10-15 00:41:58 +0900

    commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, "./build")));
+
+app.get('*', (req, res, next)=>{
+  res.sendFile(path.join(__dirname, "./build", "index.html"));
+});
+app.listen({ port: process.env.PORT || 4000 }, () =>
+  console.log(`Server ready ${process.env.PORT || 4000}`)
+);
